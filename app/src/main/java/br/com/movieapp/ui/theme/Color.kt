0d7b9063a84dad20c632a2e6e686c46bcb2ab5ba
Daniel Lopes <danieljfsantos@gmail.com>
2023-06-15 14:13:40 -0300

    Initial commit

---
+++ app/src/main/java/br/com/movieapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package br.com.movieapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlackPrimary = Color(0xFF000000)
+val YellowSecondary = Color(0xFFFFAC33)
+val BlackSecondary = Color(0xFF000000)
+
+val black = Color(0xFF000000)
+val yellow = Color(0xFFFFAC33)
+val white = Color(0xFFFFFFFF)
